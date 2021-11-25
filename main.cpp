657491521f47cf71627d12d55dbd73dc2963dc4c
emivnajera <emivelasquez2000@hotmail.com>
2021-11-24 20:55:27 -0600

    se agrego el archivo de la estructura basica de c++

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//Area de Librerias
+#include <iostream>  //Libreria Usada para Ingresar o mostrar datos en la terminal de comandos
+
+using namespace std;
+
+int main() { // Declaracion de la funcion main
+//Todo el codigo del programa va dentro del main
+
+cout << "Hola Mundo";
+
+
+}
