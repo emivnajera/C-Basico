d766c0fe8ee30bd535bf006a7bae71b07b0141a4
emivnajera <emivelasquez2000@hotmail.com>
2021-11-24 21:12:51 -0600

    se agregaron variables al archivo de estructura basica.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,15 @@ using namespace std;
 int main() { // Declaracion de la funcion main
 //Todo el codigo del programa va dentro del main
 
-cout << "Hola Mundo";
+//Declaracion de Variables
+int age =  0;
 
+cout << "Hola Mundo"<<"\n"; //\n es un salto de linea
+
+cout << age<<"\n"; //Se imprime el valor de edad
+
+age = 18; //se actualizo el valor de la edad
+
+cout << age;//Se imprime el valor de edad de nuevo
 
 }
