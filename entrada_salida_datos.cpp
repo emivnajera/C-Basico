472de816b5b3c8d1fb631f317a8ad5c20480492b
emivnajera <emivelasquez2000@hotmail.com>
2021-11-26 21:07:31 -0600

    se agrego el archivo de entrada y salida de datos.

---
+++ entrada_salida_datos.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int age = 0;
+    cout<<"Ingrese su edad"<<endl;//Salida de Datos
+    cin>>age;//Entrada de datos a la variable age
+    cout<<"Su edad es: "<<age<<endl;
+}
