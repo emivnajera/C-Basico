b835c1f7c319087f216f88d9c515a9105b7585c7
emivnajera <emivelasquez2000@hotmail.com>
2021-11-30 16:43:15 -0600

    se agrego el archivo de ciclos while y do while

---
+++ CIcloDoWhile.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+    using namespace std;
+    
+    int main() {
+    /*
+    ESTRUCTURA While
+    
+    while('CONDICION'){
+        'INTRUCCIONES'
+    }
+    */
+    char op = 'n';
+    do{
+        cout << "Desea terminar?"<<endl;
+        cin >> op;
+    }while (op != 'y');
+    cout << "Orale pue";
+    }
+    
